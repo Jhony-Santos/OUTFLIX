ec393f6055b78c873336356f926a37b3b54fef6e
Jhonatan Santos <jhony.jpn@gmail.com>
2020-07-27 09:15:05 +0430

    inicio

--- php/php-setADM.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "php-ConexaoBanco.php";
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
-$User = $_SESSION["usuario"];
-mysqli_query($rcon,"UPDATE usuarios SET permissao = 10 WHERE login = '$User';");
-?>
